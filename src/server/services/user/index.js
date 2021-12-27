7dd490f017d4ac347d2a0c5ec053563481c8f46a
Rafael Lima <drdverzola@gmail.com>
2021-12-27 14:34:15 +0000

    added middleware authorizeUser to the routes

--- src/server/services/user/index.js
+++ src/server/services/user/index.js
@@ -1,11 +1,18 @@
 import express from 'express'
-
+import FirebaseAuthService from '../../../firebase/firebase_auth.js'
 import userHandlers from './user_handlers.js'
 
-const { create, isExistentUser, userValidation } = userHandlers
+const { create, isExistentUser } = userHandlers
+const { authorizeUser } = FirebaseAuthService
 
 const router = express.Router()
 
-router.route('/').post(userValidation, isExistentUser, create)
+router
+    .route('/')
+    .post(authorizeUser, isExistentUser, create)
+    .put(authorizeUser, (req, res, next) => {
+        console.log(req._uid, req.email)
+    })
+    .delete()
 
 export default router
