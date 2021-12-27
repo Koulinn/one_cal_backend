9d8617cb557ac90887b9e6b58c79c876868242f9
Rafael Lima <drdverzola@gmail.com>
2021-12-27 11:50:03 +0000

    added validation on new user

--- src/server/services/user/user_handlers.js
+++ src/server/services/user/user_handlers.js
@@ -1,6 +1,10 @@
 import user_queries from '../../../DB/Entities/user/query_handler.js'
+import lib from '../../../library/index.js'
 
-const { insert_user } = user_queries
+const { insert_user, check_existent_user } = user_queries
+const {
+    aux: { errorMsgOnValidation },
+} = lib
 
 const create = async (req, res, next) => {
     try {
@@ -16,8 +20,51 @@ const create = async (req, res, next) => {
     }
 }
 
+const isExistentUser = async (req, res, next) => {
+    try {
+        const { email, uid } = req.body
+        const DB_res = await check_existent_user(email, uid)
+        const isExistentUser = DB_res.rowCount
+
+        if (isExistentUser) {
+            res.status(400).send({ msg: 'E-mail already registered', email })
+        } else {
+            next()
+        }
+    } catch (error) {
+        res.status(500).send({ msg: 'Server error' })
+    }
+}
+
+const userValidation = (req, res, next) => {
+    const expectedProperties = ['email', 'uid']
+    let checked = 0
+    const isValid = 2
+    let missingPropertiesText = `The properties are missing`
+
+    expectedProperties.forEach((property) => {
+        if (req.body[property] !== undefined) {
+            checked++
+        } else {
+            missingPropertiesText += ` ${property},`
+        }
+    })
+
+    if (isValid === checked) {
+        next()
+    } else {
+        // const lastComa = missingProperties.lastIndexOf(',')
+        // const msg = missingProperties.slice(0, lastComa) + '.'
+        const msg = errorMsgOnValidation(missingPropertiesText)
+
+        res.status(400).send({ msg })
+    }
+}
+
 const userHandlers = {
     create,
+    isExistentUser,
+    userValidation,
 }
 
 export default userHandlers
