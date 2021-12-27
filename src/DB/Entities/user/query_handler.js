abc7a6d4c9d1945b0b942fa4b04f3ca2f1e411cc
Rafael Lima <drdverzola@gmail.com>
2021-12-27 11:48:45 +0000

    check existent user query

--- src/DB/Entities/user/query_handler.js
+++ src/DB/Entities/user/query_handler.js
@@ -8,10 +8,24 @@ const insert_user = async (email, uid) => {
         const DB_res = await client.query(query, values)
         return DB_res
     } catch (error) {
+        console.log('========= Error from insert_user ======== ')
         console.log(error)
     }
 }
 
-const query_handler = { insert_user }
+const check_existent_user = async (email, uid) => {
+    const query = 'SELECT email, uid FROM users WHERE email = $1 OR uid = $2'
+    const values = [email, uid]
+
+    try {
+        const DB_res = await client.query(query, values)
+        return DB_res
+    } catch (error) {
+        console.log('========= Error from check_existent_user ======== ')
+        console.log(error)
+    }
+}
+
+const query_handler = { insert_user, check_existent_user }
 
 export default query_handler
