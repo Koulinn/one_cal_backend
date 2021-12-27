baa98f4e91652e5f9021b9d09441ec4b1b2ce694
Rafael Lima <drdverzola@gmail.com>
2021-12-27 19:35:45 +0000

    added editUser

--- src/server/services/user/index.js
+++ src/server/services/user/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import FirebaseAuthService from '../../../firebase/firebase_auth.js'
 import userHandlers from './user_handlers.js'
 
-const { create, isExistentUser } = userHandlers
+const { create, isExistentUser, editUser, deleteUser } = userHandlers
 const { authorizeUser } = FirebaseAuthService
 
 const router = express.Router()
@@ -10,9 +10,7 @@ const router = express.Router()
 router
     .route('/')
     .post(authorizeUser, isExistentUser, create)
-    .put(authorizeUser, (req, res, next) => {
-        console.log(req._uid, req.email)
-    })
-    .delete()
+    .put(authorizeUser, isExistentUser, editUser)
+    .delete(authorizeUser, isExistentUser, deleteUser)
 
 export default router
