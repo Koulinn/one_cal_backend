fc70d0cd4606413631334f4705c83799b977ca78
Rafael Lima <drdverzola@gmail.com>
2021-12-27 12:34:22 +0000

    refactored to have a single reading query function

--- src/DB/Entities/user/query_handler.js
+++ src/DB/Entities/user/query_handler.js
@@ -1,9 +1,6 @@
 import client from '../../db_config.js'
 
-const insert_user = async (email, uid) => {
-    const query = 'INSERT INTO users(email, uid) VALUES($1, $2) RETURNING email, uid'
-    const values = [email, uid]
-
+const read_query = async (query, values) => {
     try {
         const DB_res = await client.query(query, values)
         return DB_res
@@ -13,19 +10,6 @@ const insert_user = async (email, uid) => {
     }
 }
 
-const check_existent_user = async (email, uid) => {
-    const query = 'SELECT email, uid FROM users WHERE email = $1 OR uid = $2'
-    const values = [email, uid]
-
-    try {
-        const DB_res = await client.query(query, values)
-        return DB_res
-    } catch (error) {
-        console.log('========= Error from check_existent_user ======== ')
-        console.log(error)
-    }
-}
-
-const query_handler = { insert_user, check_existent_user }
+const query_handler = { read_query }
 
 export default query_handler
