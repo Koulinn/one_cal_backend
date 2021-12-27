67691bd3ce88d07b3ab7d939204cf10e25f1aeeb
Rafael Lima <drdverzola@gmail.com>
2021-12-27 19:37:16 +0000

    changed to sequelize query reader

--- src/DB/Entities/user/query_handler.js
+++ src/DB/Entities/user/query_handler.js
@@ -1,11 +1,11 @@
 import client from '../../db_config.js'
 
-const read_query = async (query, values) => {
+const read_query = async (query) => {
     try {
-        const DB_res = await client.query(query, values)
+        const DB_res = await client.query(query)
         return DB_res
     } catch (error) {
-        console.log('========= Error from insert_user ======== ')
+        console.log('========= Error from read query ======== ')
         console.log(error)
     }
 }
