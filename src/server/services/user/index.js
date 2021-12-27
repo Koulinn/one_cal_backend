ab78a4b0b0481a380fbd2772b471192a8049839e
Rafael Lima <drdverzola@gmail.com>
2021-12-27 11:49:38 +0000

    added user validation

--- src/server/services/user/index.js
+++ src/server/services/user/index.js
@@ -2,10 +2,10 @@ import express from 'express'
 
 import userHandlers from './user_handlers.js'
 
-const { create } = userHandlers
+const { create, isExistentUser, userValidation } = userHandlers
 
 const router = express.Router()
 
-router.route('/').post(create)
+router.route('/').post(userValidation, isExistentUser, create)
 
 export default router
