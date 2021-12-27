0511aefbd7edfd5149ae1638f8bd1b7f687a654d
Rafael Lima <drdverzola@gmail.com>
2021-12-27 19:47:40 +0000

    added on Cascade effect to user_meal table

--- src/DB/Entities/user_meal/user_mealTable.sql
+++ src/DB/Entities/user_meal/user_mealTable.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS user_meal(
     user_id varchar(130) not null,
     meal_id INTEGER,
     CONSTRAINT user_meal_PK PRIMARY KEY ("user_id", "meal_id"),
-    CONSTRAINT user_FK FOREIGN KEY (user_id) REFERENCES user,
+    CONSTRAINT user_FK FOREIGN KEY (user_id) REFERENCES user ON DELETE CASCADE,
     CONSTRAINT meal_FK FOREIGN KEY (meal_id) REFERENCES meal
 );
