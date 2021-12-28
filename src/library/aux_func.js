3b50e38acf1ecb38287a6892402ccc3c9ba86fb3
Rafael Lima <drdverzola@gmail.com>
2021-12-28 11:54:29 +0000

    send read_query to lib

--- src/library/aux_func.js
+++ src/library/aux_func.js
@@ -1,3 +1,16 @@
-const aux = {}
+import client from '../DB/db_config.js'
+
+const read_query = async (query) => {
+    try {
+        const DB_res = await client.query(query)
+        return DB_res
+    } catch (error) {
+        console.log('======== Error from read query ======== ')
+        console.log(error)
+        throw '======== Error from read_query ========'
+    }
+}
+
+const aux = { read_query }
 
 export default aux
