e6f6f4dd73ceea19b7da9c074d946c5804311fb2
Rafael Lima <drdverzola@gmail.com>
2021-12-28 14:26:24 +0000

    fixed user_meal table adding time_eaten

--- src/DB/Entities/user_meal/user_mealTable.sql
+++ src/DB/Entities/user_meal/user_mealTable.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS user_meal(
+    user_meal_id SERIAL not null,
     user_id varchar(130) not null,
     meal_id INTEGER not null,
+    time_eaten TIMESTAMP not null,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP not null,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP not null,
-    CONSTRAINT user_meal_PK PRIMARY KEY ("user_id", "meal_id"),
+    CONSTRAINT user_meal_PK PRIMARY KEY (user_meal_id),
     CONSTRAINT user_FK FOREIGN KEY (user_id) REFERENCES users ON DELETE CASCADE,
     CONSTRAINT meal_FK FOREIGN KEY (meal_id) REFERENCES meal
 );
