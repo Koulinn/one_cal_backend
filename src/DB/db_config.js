2a1af3a1dcb6d77f955fcb7088d01e829de4c114
Rafael Lima <drdverzola@gmail.com>
2021-12-28 11:53:10 +0000

    check which DB is being connected

--- src/DB/db_config.js
+++ src/DB/db_config.js
@@ -9,6 +9,8 @@ const { PGUSER, PGHOST, PGPASSWORD, PGPORT } = process.env
 
 export const database = APP_MODE === 'development' ? 'one_cal_dev' : 'one_cal'
 
+console.log(database, '<<<<<<<database connected')
+
 const db_config = {
     database: database,
     username: PGUSER,
