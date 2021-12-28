2c9244a7b75049af5632932036f7b3d008bea048
Rafael Lima <drdverzola@gmail.com>
2021-12-28 11:53:29 +0000

    added constraind to name unique

--- src/DB/Entities/meal/mealTable.sql
+++ src/DB/Entities/meal/mealTable.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS meal(
     _id SERIAL,
     name varchar(80) not null,
     calories INTEGER not null,
-    CONSTRAINT meal_PK PRIMARY KEY (_id)
+    CONSTRAINT meal_PK PRIMARY KEY (_id),
+    CONSTRAINT unique_name UNIQUE (name)
 );
