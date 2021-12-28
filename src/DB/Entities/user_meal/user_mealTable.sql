2cbd3541631b17eca93fcdacec6a5d43222d8ca4
Rafael Lima <drdverzola@gmail.com>
2021-12-28 11:55:12 +0000

    fixed typo on line 5 and added not null to meal_id

--- src/DB/Entities/user_meal/user_mealTable.sql
+++ src/DB/Entities/user_meal/user_mealTable.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS user_meal(
     user_id varchar(130) not null,
-    meal_id INTEGER,
+    meal_id INTEGER not null,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP not null,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP not null,
     CONSTRAINT user_meal_PK PRIMARY KEY ("user_id", "meal_id"),
-    CONSTRAINT user_FK FOREIGN KEY (user_id) REFERENCES user ON DELETE CASCADE,
+    CONSTRAINT user_FK FOREIGN KEY (user_id) REFERENCES users ON DELETE CASCADE,
     CONSTRAINT meal_FK FOREIGN KEY (meal_id) REFERENCES meal
 );
