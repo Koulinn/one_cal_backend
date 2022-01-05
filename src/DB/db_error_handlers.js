acd9a7b3150ea6f7a77101df84d2dda83a7e6018
Rafael Lima <drdverzola@gmail.com>
2022-01-05 10:21:48 +0000

    removed unnecessary error handler

--- src/DB/db_error_handlers.js
+++ src/DB/db_error_handlers.js
@@ -1,17 +1,7 @@
-import { database } from './db_config.js'
-
 const db_conn_resolve = (err) => {
-  if (err) {
-    console.error('connection error', err.stack)
-  } else {
-    console.log('DB connected at ' + database)
-  }
+    console.error('connection error', err)
 }
 
-const db_generic_error = (err) => {
-  console.error('connection error', err.stack)
-}
-
-const db_error_handlers = { db_conn_resolve, db_generic_error }
+const db_error_handlers = { db_conn_resolve }
 
 export default db_error_handlers
