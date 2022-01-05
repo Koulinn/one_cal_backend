400d73f27d4c89310079a383e94550f86722a119
Rafael Lima <drdverzola@gmail.com>
2022-01-05 10:21:33 +0000

    added table_creation_queries

--- src/DB/db_connection.js
+++ src/DB/db_connection.js
@@ -1,14 +1,16 @@
 import sequelize from './db_config.js'
 import db_error_handlers from './db_error_handlers.js'
+import table_creation_queries from './Entities/index.js'
 
-const { db_conn_resolve, db_generic_error } = db_error_handlers
+const { db_conn_resolve } = db_error_handlers
 
 const db_connect = async () => {
     try {
         await sequelize.authenticate()
-        console.log('Connection has been established successfully.')
+        await sequelize.query(table_creation_queries)
+        console.log('Connection to DB has been established successfully.')
     } catch (error) {
-        console.error('Unable to connect to the database:', error)
+        db_conn_resolve(error)
     }
 }
 
