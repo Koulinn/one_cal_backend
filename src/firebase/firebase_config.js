c79aebda4220e6f71622d40e2afc409af2d78695
Rafael Lima <drdverzola@gmail.com>
2022-01-22 10:07:07 +0000

    configure firebase with storage

--- src/firebase/firebase_config.js
+++ src/firebase/firebase_config.js
@@ -1,18 +1,37 @@
 import admin from 'firebase-admin'
+import { getStorage } from 'firebase/storage'
+import { initializeApp } from 'firebase/app'
 
-const { FIREBASE_PROJECT_ID } = process.env
+const {
+    FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET,
+    FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN,
+    FIREBASE_APP_ID,
+    FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_MEASUREMENT_ID,
+} = process.env
 
 const config = {
     projectId: FIREBASE_PROJECT_ID,
     credential: admin.credential.applicationDefault(),
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
+    measurementId: FIREBASE_MEASUREMENT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
 }
 
+const firebaseStore = initializeApp(config)
 const firebaseApp = admin.initializeApp(config)
 
 const auth = firebaseApp.auth()
+const storage = getStorage(firebaseStore)
 
 const firebaseConfig = {
     auth,
+    storage,
 }
 
 export default firebaseConfig
