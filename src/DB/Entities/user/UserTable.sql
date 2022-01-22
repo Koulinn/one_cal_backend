1cf73f06a6df8d7be4f282dfa99f6a61eb266a51
Rafael Lima <drdverzola@gmail.com>
2022-01-22 10:06:51 +0000

    added avatar to users table

--- src/DB/Entities/user/UserTable.sql
+++ src/DB/Entities/user/UserTable.sql
@@ -2,7 +2,12 @@ CREATE TABLE IF NOT EXISTS users(
     name varchar(40),
     surname varchar(80),
     birth_date date,
-    email varchar(80) not null,
-    uid varchar(130) not null,
+    email varchar(80) NOT NULL,
+    uid varchar(130) NOT NULL,
     CONSTRAINT user_PK PRIMARY KEY (uid)
 );
+
+ALTER TABLE
+    users
+ADD
+    COLUMN avatar VARCHAR(244);
